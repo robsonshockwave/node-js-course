552bcc44cd2d88bd3068d593c210d7ee9edd5798
Robson Arruda <robsonshockwavegithub@gmail.com>
2023-01-05 00:06:15 -0300

    delete/arquivo

--- 06-cli/database.js
+++ 06-cli/database.js
@@ -41,6 +41,21 @@ class Database {
     const dadosFiltrados = dados.filter((item) => (id ? item.id === id : true));
     return dadosFiltrados;
   }
+
+  async remover(id) {
+    if (!id) {
+      await this.escreverArquivo([]);
+      return true;
+    }
+
+    const dados = await this.obterDadosArquivo();
+    const indice = dados.findIndex((item) => item.id === parseInt(id));
+    if (indice === -1) {
+      throw Error('O usuario informado não existe');
+    }
+    dados.splice(indice, 1);
+    return await this.escreverArquivo(dados);
+  }
 }
 
 module.exports = new Database();
--- 06-cli/test.js
+++ 06-cli/test.js
@@ -27,4 +27,11 @@ describe('Suite de manipulação de Herois', () => {
 
     deepEqual(actual, expected);
   });
+
+  // it.only -> só roda esse teste
+  it('deve remover um heroi por id', async () => {
+    const expected = true;
+    const resultado = await database.remover(DEFAULT_ITEM_CADASTRAR.id);
+    deepEqual(resultado, expected);
+  });
 });

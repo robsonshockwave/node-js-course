a6cfe4210c181d8f43d691d413a51ec9a9c1acf0
Robson Arruda <robsonshockwavegithub@gmail.com>
2023-01-04 23:54:56 -0300

    create/arquivo

--- 06-cli/test.js
+++ 06-cli/test.js
@@ -2,13 +2,29 @@ const { deepEqual, ok } = require('assert');
 
 const database = require('./database');
 
-const DEFAULT_ITEM_CADASTRAR = { nome: 'Flash', poder: 'Speed', id: 1 };
+const DEFAULT_ITEM_CADASTRAR = { nome: 'Batman', poder: 'Speed', id: 2 };
 
 describe('Suite de manipulação de Herois', () => {
+  before(async () => {
+    await database.cadastrar(DEFAULT_ITEM_CADASTRAR);
+  });
+
   it('deve pesquisar um heroi usando arquivos', async () => {
     const expected = DEFAULT_ITEM_CADASTRAR;
     const [resultado] = await database.listar(expected.id);
 
     deepEqual(resultado, expected);
+  });
+
+  it('deve cadastrar um heroi, usando arquivos', async () => {
+    const expected = {
+      ...DEFAULT_ITEM_CADASTRAR,
+      id: 2,
+      nome: 'Batman',
+    };
+    const resultado = await database.cadastrar(DEFAULT_ITEM_CADASTRAR);
+    const [actual] = await database.listar(DEFAULT_ITEM_CADASTRAR.id);
+
+    deepEqual(actual, expected);
   });
 });
